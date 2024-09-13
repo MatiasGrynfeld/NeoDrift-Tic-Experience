4728d4b675f1bceead91b8539fe58fcb448d602c
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-09-12 21:10:31 -0300

    load and send config script for ws server done

---
+++ Front-End/Scripts/load-send-config.js
@@ -0,0 +1,28 @@
+let ws;
+
+window.onload = function() {
+    ws = new WebSocket("wss://example.com/ws");
+    ws.onopen = function() {
+        console.log("Connected to WebSocket");
+    };
+    ws.onmessage = function(event) {
+        console.log(event.data);
+    };
+    ws.onclose = function() {
+        console.log("WebSocket connection closed");
+    };
+    ws.onerror = function(error) {
+        console.log("WebSocket error: " + error);
+    };
+};
+
+const btn_enviar = document.getElementById('boton-enviar');
+btn_enviar.addEventListener('click', sendMessage);
+function sendMessage() {
+    let input = colores;
+    if (ws.readyState === WebSocket.OPEN) {
+        ws.send(input.JSON.stringify());
+    } else {
+        console.log("WebSocket is not connected.");
+    }
+}
