3a6249a0c21c0acd7649d350f1c66695e9a78de0
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-10-04 13:10:46 -0300

    agrego botón de conectar

--- Front-End/Scripts/load-send-config.js
+++ Front-End/Scripts/load-send-config.js
@@ -1,18 +1,27 @@
 let ws;
+const form_conectar = document.getElementById('form-conectar');
 
-window.onload = function() {
+form_conectar.addEventListener('submit', (event) => {
+    conectar(event);
+});
+
+function conectar(event) {
+    event.preventDefault();
     ws = new WebSocket("ws://127.0.0.1:8000/ws/1");
     ws.onopen = function() {
         console.log("Connected to WebSocket");
+        alert("Conexión exitosa.");
     };
     ws.onmessage = function(event) {
         console.log(event.data);
     };
     ws.onclose = function() {
         console.log("WebSocket connection closed");
+        alert("Conexión cerrada o no disponible.");
     };
     ws.onerror = function(error) {
         console.log("WebSocket error: " + error);
+        alert("Error en la conexión.");
     };
 };
 
@@ -22,9 +31,16 @@ btn_enviar.addEventListener('click', sendMessage);
 function sendMessage(event) {
     event.preventDefault();
     let input = colores[perfil];
+    if (!ws) {
+        console.log("WebSocket is not connected.");
+        alert("No hay conexión.");
+        return;
+    }
     if (ws.readyState === WebSocket.OPEN) {
         ws.send(JSON.stringify(input));
+        alert("Configuración enviada correctamente.");
     } else {
         console.log("WebSocket is not connected.");
+        alert("No hay conexión.");
     }
 }
